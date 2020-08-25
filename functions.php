4593162ee385550ce9b9eb6dca9c25677234fc63
Eli Finer <eli.finer@gmail.com>
2020-08-25 01:38:00 -0400

    add post footer widget area

--- functions.php
+++ functions.php
@@ -1,7 +1,30 @@
 <?php
 
+add_filter( 'the_content', 'content_footer' );
 add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
+add_action( 'widgets_init', 'widgets_init' );
 
 function enqueue_parent_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+}
+
+function content_footer( $content ) {    
+    // add a custom widget area to add some content to every post (usually a CTA)
+    ob_start();
+    dynamic_sidebar("Post Footer");
+    $sidebar = ob_get_clean();
+    $content .= '<p>'.$sidebar.'</p>';
+    return $content;
+}
+
+
+function widgets_init() {
+    register_sidebar(array(
+        'name' => 'Post Footer',
+        'id' => 'post-footer',
+        'before_widget' => '<div class = "widgetizedArea">',
+        'after_widget' => '</div>',
+        'before_title' => '<h3>',
+        'after_title' => '</h3>',
+    ));
 }
