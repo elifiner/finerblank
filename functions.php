dc81ced7a22835aa870d449bd23291a7120a23f9
Eli Finer <eli.finer@gmail.com>
2020-11-07 22:22:02 -0500

    fix editor bullets

--- functions.php
+++ functions.php
@@ -2,10 +2,15 @@
 
 add_filter( 'the_content', 'content_footer' );
 add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
+add_action( 'admin_enqueue_scripts', 'load_custom_wp_admin_style' );
 add_action( 'widgets_init', 'widgets_init' );
 
 function enqueue_parent_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+}
+
+function load_custom_wp_admin_style(){
+    wp_enqueue_style('custom_wp_admin_css', get_stylesheet_directory_uri().'/admin.css');
 }
 
 function content_footer( $content ) {    
